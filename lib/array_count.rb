4bc12fcb070595f64b7fc83d56be6271bfcbfb02
Scot Sexton <rss3509@gmail.com>
2020-05-20 17:18:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,13 @@
-def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
-end
 
+def count_strings(array)
+  array.any?(String) { |num| num.count <  0  }
+    array + 1 
+    p count_strings(array)
+end
+  # Return the total number of strings in the provided array using the count enumerable
+
+=begin
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
+=end
