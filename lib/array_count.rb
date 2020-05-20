e088bc10369faf53f8df8d239ff0ac6d24515d6a
Scot Sexton <rss3509@gmail.com>
2020-05-20 17:50:52 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,8 +5,8 @@ def count_strings(array)
 end
   # Return the total number of strings in the provided array using the count enumerable
 
-=begin
+
 def count_empty_strings(array)
+  
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
-=end
